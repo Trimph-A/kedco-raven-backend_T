4b8f7009e95de1b0bd8482854061ba7530d4fabd
Joseph Ogunfowokan <josephoguns5@gmail.com>
2025-03-26 13:16:21 +0100

    Created apps for commercial, financial, hr, technical, and common

--- raven/settings.py
+++ raven/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'common',
+    'commercial',
+    'financial',
+    'technical',
+    'hr',
 ]
 
 MIDDLEWARE = [
