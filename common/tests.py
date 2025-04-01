047980c21ab1e0b9ca090ce98c5d562b51a90b5c
Joseph Ogunfowokan <josephoguns5@gmail.com>
2025-04-01 15:09:28 +0100

    Implemented tests

--- common/tests.py
+++ common/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+import pytest
+from common.models import *
 
-# Create your tests here.
+@pytest.mark.django_db
+def test_location_hierarchy():
+    state = State.objects.create(name="Lagos")
+    district = BusinessDistrict.objects.create(name="Ikeja", state=state)
+    substation = InjectionSubstation.objects.create(name="Ikeja SS", district=district)
+    feeder = Feeder.objects.create(name="Feeder 1", substation=substation)
+    transformer = DistributionTransformer.objects.create(name="Transformer A", feeder=feeder)
+
+    assert transformer.feeder == feeder
+    assert feeder.substation == substation
+    assert substation.district == district
+    assert district.state == state
