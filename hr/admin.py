455129ae1c510aab9bf2bdb9828bcf8804328b02
Joseph Ogunfowokan <josephoguns5@gmail.com>
2025-05-19 01:38:15 +0100

    Created admin for hr

--- hr/admin.py
+++ hr/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Department, Role, Staff
 
-# Register your models here.
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug',]
+    search_fields = ['name']
+
+
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'department', 'slug',]
+    search_fields = ['title', 'department']
+
+@admin.register(Staff)
+class StaffAdmin(admin.ModelAdmin):
+    list_display = ['full_name', 'email', 'phone_number', 'role', 'department', 'state', 'district']
+    search_fields = ['full_name', 'email', 'phone_number']
+    list_filter = ['grade', 'gender', 'department']
+
