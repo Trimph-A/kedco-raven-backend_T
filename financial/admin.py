ae4daab4ebfae64e62b7369fa551026509bc1bd7
Joseph Ogunfowokan <josephoguns5@gmail.com>
2025-05-19 02:09:45 +0100

    Included GLBreakdown in admin

--- financial/admin.py
+++ financial/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
-from .models import ExpenseCategory, Expense, DailyCollection, MonthlyRevenueBilled, SalesRepresentative, SalesRepPerformance
+from .models import ExpenseCategory, GLBreakdown, Expense, DailyCollection, MonthlyRevenueBilled, SalesRepresentative, SalesRepPerformance
 
 @admin.register(ExpenseCategory)
 class ExpenseCategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'is_special']
     search_fields = ['name']
+
+
+@admin.register(GLBreakdown)
+class GLBreakdownAdmin(admin.ModelAdmin):
+    list_display = ['name',]
+    search_fields = ['name',]
 
 
 @admin.register(Expense)
